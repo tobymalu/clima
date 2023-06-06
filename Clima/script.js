d68f43257b11ca89967bd08655b54c97323dfb35
tobymalu <126038894+tobymalu@users.noreply.github.com>
2023-06-06 16:21:03 -0600

    Add files via upload

---
+++ Clima/script.js
@@ -0,0 +1,74 @@
+function consultarClima(){
+    const ciudad= document.getElementById('ciudad').value;
+    const API_KEY='7988e38cdeafad1d912e3c3ab218ad65'; 
+    const url= `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${API_KEY}&lang=es`;
+    
+    fetch(url)
+    .then(response=>{
+        if(response.ok){
+            return response.json();
+        }else{
+            throw new Error('Error en la respuesta de la Api');
+        }
+    })
+    .then(data=>{
+        const tabla= document.getElementById('tabla-clima').getElementsByTagName('tbody')[0];
+        const icono=data.weather[0].icon
+        const fila= tabla.insertRow();
+        fila.insertCell().innerHTML=data.name;
+        fila.insertCell().innerHTML=`${(data.main.temp- 273.15).toFixed(1)}°C`;
+        fila.insertCell().innerHTML= data.weather[0].description;    
+        
+       let hora= new Date(data.dt*1000).toLocaleString("es-MX",{
+        timeStyle:"short"
+       });
+       fila.insertCell().innerHTML=hora;
+       fila.insertCell().innerHTML=`<img src=https://openweathermap.org/img/wn/${icono}@2x.png  alt="">`;
+      
+        
+       
+               
+    } 
+
+    
+    
+
+    )
+    .catch(error=>{
+        console.error('Error al consultar el clima', error);
+    });
+}
+function consultarClimas(){
+    const ciudades= document.getElementById('ciudades').value.split(',').map(ciudad=>ciudad.trim());
+    const API_KEY='7988e38cdeafad1d912e3c3ab218ad65'; 
+    Promise.all(ciudades.map(ciudad=>{
+        const url=`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${API_KEY}&lang=es`;
+        return fetch(url).then(response=>{
+            if(response.ok){
+                return response.json();
+            } else{
+                throw new Error('Error en la respuesta API');
+            }
+        });
+    }))
+    .then(data=>{
+        const tabla= document.getElementById('tabla-clima').getElementsByTagName('tbody')[0];
+        data.forEach(ciudad=>{
+            const iconos=ciudad.weather[0].icon
+            const fila=tabla.insertRow();
+            fila.insertCell().innerHTML=ciudad.name;
+            fila.insertCell().innerHTML=`${(ciudad.main.temp-273.15).toFixed(1)}°C`;
+            fila.insertCell().innerHTML=ciudad.weather[0].description;
+            fila.insertCell().innerHTML=`<img src=https://openweathermap.org/img/wn/${iconos}@2x.png  alt="">`;
+        
+
+        });
+    })
+    .catch(error=>{console.error('Error al consultar clima, error');});
+}
+function limpiarTabla(){
+    const tabla= document.getElementById('tabla-clima').getElementsByTagName('tbody');
+    for(let i=0; i<tabla.length;i++){
+        tabla[i].innerHTML="";
+    }
+}
